b66ea9ec1d487fc009bf8c74a12f3696e298da3b
artemivanov <artem.ivanov@almatech.dev>
2023-11-20 00:36:11 +0600

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -29,12 +29,12 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-        {/* Here we add our button with alert callback */}
-        {/*<div className="card">*/}
-        {/*    <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>*/}
-        {/*        Show Alert*/}
-        {/*    </button>*/}
-        {/*</div>*/}
+         Here we add our button with alert callback
+        <div className="card">
+            <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
+                Show Alert
+            </button>
+        </div>
     </>
   )
 }
