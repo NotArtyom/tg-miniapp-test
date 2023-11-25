228e81c107072fb3abe012b7fbf83b32d2525c5a
artemivanov <artem.ivanov@almatech.dev>
2023-11-26 01:38:35 +0600

    Header & Settings

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import WebApp from "@twa-dev/sdk";
 import {TonConnectUIProvider} from "@tonconnect/ui-react";
+import {Header} from "./components/Header";
+import {Address} from "./components/Address";
+import {Settings} from "./components/Settings";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <TonConnectUIProvider manifestUrl="https://<YOUR_APP_URL>/tonconnect-manifest.json">
+      <TonConnectUIProvider manifestUrl="https://notartyom.github.io/tg-miniapp-test/manifest.json">
+      <Header/>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -19,17 +23,13 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/*<div className="card">*/}
+      {/*  <button onClick={() => setCount((count) => count + 1)}>*/}
+      {/*    count is {count}*/}
+      {/*  </button>*/}
+      {/*</div>*/}
+      <Address/>
+      <Settings />
          Here we add our button with alert callback
         <div className="card">
             <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
